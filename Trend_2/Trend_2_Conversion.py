328e4b5e6e5cc98fc47dd1c18a8b707018dedcae
Qiyue Song <qiyuesong@Qiyues-MacBook-Pro.local>
2018-04-19 10:53:50 +0800

    update

--- Trend_2/Trend_2_Conversion.py
+++ Trend_2/Trend_2_Conversion.py
@@ -18,26 +18,46 @@ for path in path_list:
     path_str = str(path)
     parsed_path = path_str.split("/")
     conference = parsed_path[2]
-    if conference not in csv_dict:
-        csv_dict[conference] = {}
-    year = interpret_year(parsed_path[3].replace(conference, ''))
-    if year not in csv_dict[conference]:
-        csv_dict[conference][year] = {"valid": 0, "invalid": 0}
-    with open(path_str) as content:
-        publication = json.loads(content.read())
-        last_index = len(publication['algorithms']['algorithm']) - 1
-        if publication['algorithms']['algorithm'][last_index]["@name"] == 'ParsCit':
-            if publication['algorithms']['algorithm'][last_index]["citationList"] is not None:
-                citation_list = publication['algorithms']['algorithm'][last_index]["citationList"]["citation"]
-                if isinstance(citation_list, dict):
-                    citation_list = [citation_list]
-                for citation in citation_list:
-                    if citation["@valid"] == "true":
-                        csv_dict[conference][year]["valid"] += 1
-                    elif citation["@valid"] == "false":
-                        csv_dict[conference][year]["invalid"] += 1
-        else:
-            no_citation_list.append(path_str)
+    file_code = parsed_path[4].split("-")[1]
+    if not file_code.endswith("000"):
+        if conference not in csv_dict:
+            csv_dict[conference] = {}
+        year = interpret_year(parsed_path[3].replace(conference, ''))
+        if year not in csv_dict[conference]:
+            csv_dict[conference][year] = {"valid": 0, "invalid": 0}
+        with open(path_str) as content:
+            publication = json.loads(content.read())
+            if conference == 'J' and ('title' in publication['algorithms']['algorithm'][0]['variant']) \
+                and isinstance(publication['algorithms']['algorithm'][0]['variant']['title'], dict) \
+                and (publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Briefly Noted") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Publications Received") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Author Index") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Title Index") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Advertisements") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find( "Guidelines for Submission") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Calls for Papers") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Announcements") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Abstracts of Current Literature") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Membership List") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Calls for Participation") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Site Report") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Program of") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Minutes of") != -1):
+                print(path_str)
+                continue
+            last_index = len(publication['algorithms']['algorithm']) - 1
+            if publication['algorithms']['algorithm'][last_index]["@name"] == 'ParsCit':
+                if publication['algorithms']['algorithm'][last_index]["citationList"] is not None:
+                    citation_list = publication['algorithms']['algorithm'][last_index]["citationList"]["citation"]
+                    if isinstance(citation_list, dict):
+                        citation_list = [citation_list]
+                    for citation in citation_list:
+                        if citation["@valid"] == "true":
+                            csv_dict[conference][year]["valid"] += 1
+                        elif citation["@valid"] == "false":
+                            csv_dict[conference][year]["invalid"] += 1
+            else:
+                no_citation_list.append(path_str)
 
 print(len(no_citation_list))
 
--- Trend_3/Trend_3_Conversion_paper_inCitation.py
+++ Trend_3/Trend_3_Conversion_paper_inCitation.py
@@ -18,31 +18,50 @@ for path in path_list:
     path_str = str(path)
     parsed_path = path_str.split("/")
     conference = parsed_path[2]
-    if conference not in csv_dict:
-        csv_dict[conference] = {}
-    year = interpret_year(parsed_path[3].replace(conference, ''))
-    if year not in csv_dict[conference]:
-        csv_dict[conference][year] = {}
-    with open(path_str) as content:
-        publication = json.loads(content.read())
-        last_index = len(publication['algorithms']['algorithm']) - 1
-        if publication['algorithms']['algorithm'][last_index]["@name"] == 'ParsCit':
-            # print(path_str)
-            if publication['algorithms']['algorithm'][last_index]["citationList"] is not None:
-                citation_list = publication['algorithms']['algorithm'][last_index]["citationList"]["citation"]
-                if isinstance(citation_list, dict):
-                    citation_list = [citation_list]
-                for citation in citation_list:
-                    if citation["@valid"] == "true":
-                        if "title" in citation and citation["title"] is not None:
-                            paper_title = citation["title"]
-                            if paper_title not in csv_dict[conference][year]:
-                                csv_dict[conference][year][paper_title] = 0
-                            csv_dict[conference][year][paper_title] += 1
-                        else:
-                            no_citation_list.append(path_str)
-        else:
-            no_citation_list.append(path_str)
+    file_code = parsed_path[4].split("-")[1]
+    if not file_code.endswith("000"):
+        if conference not in csv_dict:
+            csv_dict[conference] = {}
+        year = interpret_year(parsed_path[3].replace(conference, ''))
+        if year not in csv_dict[conference]:
+            csv_dict[conference][year] = {}
+        with open(path_str) as content:
+            publication = json.loads(content.read())
+            if conference == 'J' and ('title' in publication['algorithms']['algorithm'][0]['variant']) \
+                and isinstance(publication['algorithms']['algorithm'][0]['variant']['title'], dict) \
+                and (publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Briefly Noted") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Publications Received") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Author Index") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Title Index") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Advertisements") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find( "Guidelines for Submission") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Calls for Papers") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Announcements") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Abstracts of Current Literature") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Membership List") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Calls for Participation") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Site Report") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Program of") != -1 \
+                or publication['algorithms']['algorithm'][0]['variant']['title']["#text"].find("Minutes of") != -1):
+                print(path_str)
+                continue
+            last_index = len(publication['algorithms']['algorithm']) - 1
+            if publication['algorithms']['algorithm'][last_index]["@name"] == 'ParsCit':
+                if publication['algorithms']['algorithm'][last_index]["citationList"] is not None:
+                    citation_list = publication['algorithms']['algorithm'][last_index]["citationList"]["citation"]
+                    if isinstance(citation_list, dict):
+                        citation_list = [citation_list]
+                    for citation in citation_list:
+                        if citation["@valid"] == "true":
+                            if "title" in citation and citation["title"] is not None:
+                                paper_title = citation["title"]
+                                if paper_title not in csv_dict[conference][year]:
+                                    csv_dict[conference][year][paper_title] = 0
+                                csv_dict[conference][year][paper_title] += 1
+                            else:
+                                no_citation_list.append(path_str)
+            else:
+                no_citation_list.append(path_str)
 
 print(len(no_citation_list))
 
